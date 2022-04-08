c3385d1eb3eeac96a0b8c4d4ce9ae7cf05035bf9
sinsin <wr1126@naver.com>
2022-04-09 00:10:17 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+const playBtn = document.getElementById("play");
+const guessForm = document.getElementById("guessForm");
+const enterBtn = document.getElementById("enter");
+const input = document.getElementById("guessInput");
+const up = document.getElementById("Up");
+const upSpan = document.querySelector("#Up span");
+const down = document.getElementById("Down");
+const downSpan = document.querySelector("#Down span");
+const retryBtn = document.getElementById("retry");
+const result = document.getElementById("result");
+const gameForm = document.getElementById("gameForm");
+const title = document.getElementById("title");
+
+const randomNumber = () => {
+  return (number = Math.round(Math.random() * 100));
+};
+
+const playHandler = () => {
+  playBtn.style.display = "none";
+  gameForm.style.display = "block";
+};
+
+const submitHandler = (event) => {
+  event.preventDefault();
+  let inputContainer = Number(input.value);
+  console.log(number);
+  input.value = "";
+
+  if (inputContainer === number) {
+    gameForm.style.display = "none";
+    title.style.display = "none";
+    result.style.display = "block";
+    retryBtn.style.display = "block";
+  } else if (inputContainer > number) {
+    upSpan.style.opacity = "0";
+    up.style.color = "gray";
+    downSpan.style.opacity = "1";
+    down.style.color = "black";
+  } else if (inputContainer < number) {
+    downSpan.style.opacity = "0";
+    down.style.color = "gray";
+    upSpan.style.opacity = "1";
+    up.style.color = "black";
+  }
+};
+
+const retryHandler = () => {
+  result.style.display = "none";
+  retryBtn.style.display = "none";
+  title.style.display = "block";
+  gameForm.style.display = "block";
+  up.style.color = "gray";
+  down.style.color = "gray";
+  upSpan.style.opacity = "0";
+  downSpan.style.opacity = "0";
+  randomNumber();
+  playHandler();
+};
+
+randomNumber();
+playBtn.addEventListener("click", playHandler);
+guessForm.addEventListener("submit", submitHandler);
+retryBtn.addEventListener("click", retryHandler);
